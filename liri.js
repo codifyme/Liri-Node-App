8c2d2f5639d6bcdd2cf1ca1baecbcbaed32fd00d
Angel Cochachez <acochachez@gmail.com>
2019-09-04 16:31:34 -0700

    Added package.json and modified liri.js

--- liri.js
+++ liri.js
@@ -17,26 +17,24 @@ var fs=require("fs");
 //command in process
 var command=process.argv[2];
 
-//Find your artist event and concert
+//Search the bands in town artist events
 if(command==="concert-this"){
-    var artist=process.argv[3];
+    var artist = process.argv[3];
     request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response,body){
         if (!error && response.statusCode===200){
             console.log("--------------------------------");
-            console.log("Venue: "+JSON.parse(body[0].venue.name);
-            console.log("Location:"+JSON.parse(body)[0].venue.city +" " +JSON.parse(body)[0].venue.region);
-            console.log("Date:"+ moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
+            console.log("Venue:" +JSON.parse(body[0].venue.name);
+            console.log("Location:" +JSON.parse(body)[0].venue.city +" " +JSON.parse(body)[0].venue.region);
+            console.log("Date:" + moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
             console.log("---------------------------");
 
         }
     });
 
-
-    //Find your favorite Song
-
+    //Search your favorite Song
 
 } else if (command==="spotify-this-song"){
-    var song=process.argv[3];
+    var song = process.argv[3];
     
     if(song==undefined){
         song="The Sign";
@@ -57,9 +55,9 @@ if(command==="concert-this"){
         console.log("--------------------------");
     });
 
-    //Find your favorite Movie info
+    //Search your favorite Movie info
 
-}else if (command ==="movie-this") {
+} else if (command ==="movie-this") {
     var movie=process.argv[3];
 
     if(movie === undefined){
@@ -74,17 +72,17 @@ if(command==="concert-this"){
         console.log("Title: " + JSON.parse(body).Title);
         console.log("Year Released: " + JSON.parse(body).Year);
         console.log("IMDB Rating: "+ JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating: "+JSON.parse(body).Rating[1].Value);
-        console.log("Country Produced: " +JSON.parse(body).Rating[1].Value);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Rating[1].Value);
+        console.log("Country Produced: " + JSON.parse(body).Rating[1].Country);
         console.log("Language: " + JSON.parse(body).Language);
         console.log("pilot: "+ JSON.parse(body).Plot);
         console.log("Actors: " + JSON.parse(body).Actors);
         console.log(" ------------------------------- ");
         }
     });
-// do what it says
-else if (command ==="do-what-it-says"){
-    fs.readFileSync("random.txt", "utf8", function (error, data){
+// DO WHAT IT SAYS Mr DUDE!
+} else if (command ==="do-what-it-says"){
+    fs.readFile("random.txt", "utf8", function (error, data){
         if (error){
             return console.log(error);
         }
@@ -100,17 +98,65 @@ else if (command ==="do-what-it-says"){
 
              request("https://rest.bandsintown.com/artist/"+ artist +"/events?app_id=codingbootcamp",function(error, response,body){
                  if(!error && response.statusCode===200){
-                     console.log('-------------------------------------------------------------------------------------------------');
+                     console.log('---------------------------------------');
                      console.log("Venue:" +JSON.parse(body)[0].venue.name);
                      console.log("Location:" + JSON.parse(body[0].venue.city+ " "+JSON.parse(body)[0].venue.region);
                      console.log("date: " + moment(JSON.parse(body)[0].datetine).format("MM/D/YYYY"));
                      console.log("-------------------------");
-                 }3
                  }
-             })
-         }
-    }
-        ))
-}
+    
+             });
+    
+            // If Mr Dude want to know about his favorite SONG do this..
 
+            } else if (command === "spotify-this-song"){
+                var song=whatTocommand;
+                if (song ===undefined){
+                    song="The Sign";
+                }
+                spotify.search({
+                    type:"track",
+                    query:song
+                }, function (err, data){
+                    if (err){
+                        return console.log("Error occured: " + err);
+                    }
+                    console.log(data.tracks.items[0].album[0]);
+                });
+
+                //If Mr Dude wants to know about his favorite MOVIE
+
+            }else if (command == "movie-this"){
+                var movie =whatToCommand;
+                if(movie==undefined){
+                    movie="Mr. Nobody";
+                }
+                request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(error, response, body){
+                    if(!error && response.statusCode===200){
+        
+                //Movie Info
+
+                console.log('-----------------------------');
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Year Released: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: "+ JSON.parse(body).imdbRating);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Rating[1].Value);
+                console.log("Country Produced: " + JSON.parse(body).Rating[1].Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("pilot: "+ JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log(" ------------------------------- ");
+                }
+            });
+            //If Command not entered or is incorrectly entered
+        } else {
+            console.log("Command Error");
+        }
+        console.log("-----------------");
+        console.log("Command: ", command);
+        console.log("-----------------");
+    });
+    // If command is entered incorrectly
+} else {
+    console.log("Command Error");
 }
